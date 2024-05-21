1226fc9cbbe98a69c74004f81c64369f1f4a44f9
LongfeiH <2069860997@qq.com>
2024-05-21 23:39:07 +0800

    22

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 // 用于为静态资源（如图像、样式表、JavaScript 文件等）设置 URL 前缀
 // 这在将应用部署到自定义域名或 CDN 上时特别有用，因为它允许您将静态资源存储在不同的位置
+repo ='vivo-aicamera.github.io'
 let assetPrefix = `/${repo}/`
 
 // 用于为应用设置基础路径
