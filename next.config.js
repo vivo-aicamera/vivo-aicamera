c60da3d5ea133104ce5d265cf190fd121fa3052b
LongfeiH <2069860997@qq.com>
2024-05-21 23:12:02 +0800

    9

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
-let assetPrefix = "/vivo-aicamera.github.io/";
-let basePath = "/vivo-aicamera.github.io";
+let assetPrefix = "";
+let basePath = "";
 
 if (isGithubActions) {
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
